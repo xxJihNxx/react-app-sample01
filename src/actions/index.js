0c8840c2a68caef612d68e6f45e04cb0bac75740
ToMo <ToMo@email.com>
2019-04-13 21:06:39 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const incremetn = () => ({
+  type: INCREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
+})
